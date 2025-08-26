4213dc0cdaa7fd041819887eadf03852633b09ff
devgupta-creates <devg12005@gmail.com>
2025-08-26 12:45:17 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,5 @@
+import math
+n= int(input("Enter a number: "))
+print(f'square root of numner is {math.sqrt(n)}')
+print(f'cube of number is {math.pow(n,3)}')
+print(f'factorial of number is {math.factorial(n)}')      
